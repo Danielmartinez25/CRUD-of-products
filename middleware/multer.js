d7fa582a966e8003eb4c3b5a30739dc079b49617
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-02 16:59:41 -0300

    create multer function

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,5 +1,13 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require("multer");
 const storage = multer.diskStorage({
-    
-})
+  destination: (req, file, cb) => {
+    cb(null, "../public/product");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `product-${Date.now()}-${file.originalname}`);
+  },
+});
+
+const uploadImage = multer({ storage });
+
+module.exports = uploadImage;
