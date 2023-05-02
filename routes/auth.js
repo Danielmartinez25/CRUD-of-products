ffbd262dd625bc3e2dc94936b97c6c13f57557bc
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-02 17:42:09 -0300

    multer middleware used

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router  = express.Router()
-
 const {list,create,update,deleted} = require('../controller/productsController')
+const  multer= require('../middlewares/multer')
 router
   .get("/list/:id", list)
-  .post("/create", create)
+  .post("/create",multer.single('file'), create)
   .put("/update/:id", update)
   .delete("/delete/:id", deleted)
   
