b7926859e46e80a35b6d0610fe7fc4a54e5c40c5
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-03 19:53:00 -0300

    update controller

--- controller/productsController.js
+++ controller/productsController.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   listProduct: async (req, res) => {
     try {
-      const product = await Product.find().select('name price');
+      const product = await Product.find().select("name price");
       return res.status(200).json({
         ok: true,
         status: 200,
@@ -64,16 +64,23 @@ module.exports = {
   },
   update: async (req, res) => {
     try {
-      const {id} = req.params
-      const product = Product.updateOne({_id : id})
-      console.log(product)
-            return res.status(200).json({
-              ok: true,
-              status: 200,
-              data: product,
-            });
+      const { id } = req.params;
+      if (!ObjectId.isValid(id)) throw createError(400, "No es un ID válido");
+      const product = await Product.findById(id);
+      if (!product) throw createError(404, "Producto no encontrado");
+      const {name,price,discount,description} = req.body;
+      product.name = name ;
+      product.price = price ;
+      product.discount = discount ;
+      product.description = description ;
+      const productUpdate = await product.save()
+      return res.status(200).json({
+        ok: true,
+        status: 200,
+        data: productUpdate,
+      });
     } catch (error) {
-      return errorStatus(res,error, 'Update')
+      return errorStatus(res, error, "Update");
     }
   },
   deleted: async (req, res) => {
