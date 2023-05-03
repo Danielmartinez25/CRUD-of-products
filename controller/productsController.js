ba85314c163f300c975ebe9feac1b249c8642103
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-03 18:48:12 -0300

    busqueda por id

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,10 +1,14 @@
 const Product = require("../database/models/product");
 const errorStatus = require("../helpers/errorStatus");
 const createError = require("http-errors");
+const ObjectId = require("mongoose").Types.ObjectId;
+
 module.exports = {
   list: async (req, res) => {
     try {
-      const product = await Product.find();
+      const { id } = req.params;
+      if (!ObjectId.isValid(id)) throw createError(400, "No es un ID válido");
+      const product = await Product.findById(id);
       return res.status(200).json({
         ok: true,
         status: 200,
@@ -16,25 +20,25 @@ module.exports = {
   },
   create: async (req, res) => {
     try {
-      const { name, price, discount, description} = req.body;
-            if (
-              [name, description, price,discount].includes("") ||
-              !name ||
-              !description ||
-              !discount ||
-              !price
-            )
-              throw createError(400, "Todos los campos son obligatorios");
+      const { name, price, discount, description } = req.body;
+      if (
+        [name, description, price, discount].includes("") ||
+        !name ||
+        !description ||
+        !discount ||
+        !price
+      )
+        throw createError(400, "Todos los campos son obligatorios");
       const product = Product({
         name,
         price,
         discount,
         description,
       });
-      if(req.file) {
-        console.log(req.file.filename)
-        const {filename} = req.file
-        product.setImgUrl(filename)
+      if (req.file) {
+        console.log(req.file.filename);
+        const { filename } = req.file;
+        product.setImgUrl(filename);
       }
       const productStore = await product.save();
       return res.status(200).json({
@@ -43,7 +47,7 @@ module.exports = {
         data: productStore,
       });
     } catch (error) {
-      return errorStatus(res,error,'Create')
+      return errorStatus(res, error, "Create");
     }
   },
   update: async (req, res) => {
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router  = express.Router()
 const {list,create,update,deleted} = require('../controller/productsController')
 const image = require("../middlewares/multer");
 router
-  .get("/list", list)
+  .get("/list/:id", list)
   .post("/create", image.single("file"), create)
   .put("/update/:id", update)
   .delete("/delete/:id", deleted);
